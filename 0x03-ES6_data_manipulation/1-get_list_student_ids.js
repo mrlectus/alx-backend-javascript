41e9fee7b27fa18dce241de0a79dca1622b16cc6
BrownCarter <lilbrown3000@gmail.com>
2023-10-04 16:12:44 +0100

    eslint fix

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,2 @@
-const getListStudentIds = (args) => {
-  return args.map((value) => value.id);
-};
-console.log(getListStudentIds("hello"))
+const getListStudentIds = (args) => args.map((value) => value.id);
+export default getListStudentIds;
